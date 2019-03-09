7ab0312cdad5ce327080eb5058f3ad4caa6dd7bc
Richard Burton <richard@balance.io>
2019-03-09 20:51:55 +0000

    Add a tab menubar.

---
+++ Balance/TabBarController.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarController.swift
+//  Balance
+//
+//  Created by Richard Burton on 3/9/19.
+//  Copyright © 2019 Balance. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let watchlistViewController = HomeViewController()
+        
+        watchlistViewController.tabBarItem = UITabBarItem(title: "Watchlist", image: UIImage(named: "watchlistTabBarItemImage.png"), selectedImage: UIImage(named: "watchlistTabBarItemImageSelected.png"))
+        
+        let balanceViewController = HomeViewController()
+        balanceViewController.tabBarItem = UITabBarItem(title: "Balance", image: UIImage(named: "balanceTabBarItemImage.png"), selectedImage: UIImage(named: "balanceTabBarItemImageSelected.png"))
+        
+        let settingsViewController = HomeViewController()
+        settingsViewController.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(named: "settingsTabBarItemImage.png"), selectedImage: UIImage(named: "settingsTabBarItemImageSelected.png"))
+        
+        let tabBarList = [watchlistViewController, balanceViewController, settingsViewController]
+        
+        viewControllers = tabBarList
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
