692f3c4f12ce026fc8d519b0ff6194ee7e02ba71
Ben Baron <ben@einsteinx2.com>
2019-03-10 11:36:21 -0500

    Cleaned up TabBarController

--- Balance/View Controllers/TabBarController.swift
+++ Balance/View Controllers/TabBarController.swift
@@ -9,34 +9,16 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    let watchlistViewController = WatchlistViewController()
+    let balanceViewController = HomeViewController()
+    let settingsViewController = SettingsViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let watchlistViewController = WatchlistViewController()
         
-        watchlistViewController.tabBarItem = UITabBarItem(title: "Watchlist", image: UIImage(named: "watchlistTabBarItemImage.png"), selectedImage: UIImage(named: "watchlistTabBarItemImageSelected.png"))
-        
-        let balanceViewController = HomeViewController()
-        balanceViewController.tabBarItem = UITabBarItem(title: "Balance", image: UIImage(named: "balanceTabBarItemImage.png"), selectedImage: UIImage(named: "balanceTabBarItemImageSelected.png"))
-        
-        let settingsViewController = SettingsViewController()
-        settingsViewController.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(named: "settingsTabBarItemImage.png"), selectedImage: UIImage(named: "settingsTabBarItemImageSelected.png"))
-        
-        let tabBarList = [watchlistViewController, balanceViewController, settingsViewController]
-        
-        viewControllers = tabBarList
+        watchlistViewController.tabBarItem = UITabBarItem(title: "Watchlist", image: UIImage(named: "watchlistTabBarItemImage"), selectedImage: UIImage(named: "watchlistTabBarItemImageSelected"))
+        balanceViewController.tabBarItem = UITabBarItem(title: "Balance", image: UIImage(named: "balanceTabBarItemImage"), selectedImage: UIImage(named: "balanceTabBarItemImageSelected"))
+        settingsViewController.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(named: "settingsTabBarItemImage"), selectedImage: UIImage(named: "settingsTabBarItemImageSelected"))
+        viewControllers = [watchlistViewController, balanceViewController, settingsViewController]
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
