499e421be7698940ded63d717ebf5068f8575c0e
Ben Baron <ben@einsteinx2.com>
2019-03-11 13:45:46 -0500

    Settings tab UI done

---
+++ Balance/Table Cells/SettingsTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  SettingsTableViewCell.swift
+//  Balance
+//
+//  Created by Benjamin Baron on 3/11/19.
+//  Copyright © 2019 Balance. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class SettingsTableViewCell: UITableViewCell {
+    var isEnabled: Bool {
+        didSet {
+            backgroundColor = isEnabled ? .white : .gray
+        }
+    }
+    
+    let sideLabel: UILabel = {
+        let sideLabel = UILabel()
+        sideLabel.translatesAutoresizingMaskIntoConstraints = false
+        sideLabel.font = UIFont.systemFont(ofSize: 17)
+        sideLabel.textColor = UIColor.init(hexString: "#8E8E93")
+        return sideLabel
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        isEnabled = true
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
+        accessoryType = .disclosureIndicator
+        textLabel?.textColor = .black
+        textLabel?.font = UIFont.systemFont(ofSize: 17.4)
+        
+        addSubview(sideLabel)
+        sideLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-40)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("unimplemented")
+    }
+}
--- Balance/View Controllers/SettingsViewController.swift
+++ Balance/View Controllers/SettingsViewController.swift
@@ -1,9 +1,269 @@
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private let balanceLogoImageView: UIImageView = {
+        let balanceLogoImageView = UIImageView()
+        balanceLogoImageView.translatesAutoresizingMaskIntoConstraints = false
+        balanceLogoImageView.image = UIImage(named: "balanceLogo")
+        return balanceLogoImageView
+    }()
+    
+    private let balanceNameImageView: UIImageView = {
+        let balanceNameImageView = UIImageView()
+        balanceNameImageView.translatesAutoresizingMaskIntoConstraints = false
+        balanceNameImageView.image = UIImage(named: "balanceName")
+        return balanceNameImageView
+    }()
+    
+    private let linesImageView: UIImageView = {
+        let linesImageView = UIImageView()
+        linesImageView.translatesAutoresizingMaskIntoConstraints = false
+        linesImageView.image = UIImage(named: "lines")
+        return linesImageView
+    }()
+    
+    private let richardImageView: UIImageView = {
+        let richardImageView = UIImageView()
+        richardImageView.translatesAutoresizingMaskIntoConstraints = false
+        richardImageView.image = UIImage(named: "richard")
+        return richardImageView
+    }()
+    
+    private let joinTitleLabel: UILabel = {
+        let joinTitleLabel = UILabel()
+        joinTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        joinTitleLabel.font = UIFont.systemFont(ofSize: 24)
+        joinTitleLabel.textColor = UIColor.init(hexString: "#191817")
+        joinTitleLabel.text = "Join the community"
+        return joinTitleLabel
+    }()
+    
+    private let joinSubtitleLabel: UILabel = {
+        let joinSubtitleLabel = UILabel()
+        joinSubtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        joinSubtitleLabel.font = UIFont.systemFont(ofSize: 17)
+        joinSubtitleLabel.textColor = UIColor.init(hexString: "#191817")
+        joinSubtitleLabel.numberOfLines = 2
+        joinSubtitleLabel.lineBreakMode = .byWordWrapping
+        joinSubtitleLabel.textAlignment = .center
+        joinSubtitleLabel.text = "Creating interfaces for the open source financial system"
+        return joinSubtitleLabel
+    }()
+    
+    private let mediumButton: UIButton = {
+        let mediumButton = UIButton()
+        mediumButton.translatesAutoresizingMaskIntoConstraints = false
+        mediumButton.setBackgroundImage(UIImage(named: "socialButtonBackground"), for: .normal)
+        mediumButton.setImage(UIImage(named: "mediumLogo"), for: .normal)
+        mediumButton.setTitle("Medium", for: .normal)
+        mediumButton.setTitleColor(UIColor.init(hexString: "#191817"), for: .normal)
+        mediumButton.titleLabel?.font = UIFont.systemFont(ofSize: 17.4)
+        return mediumButton
+    }()
+    
+    private let discordButton: UIButton = {
+        let discordButton = UIButton()
+        discordButton.translatesAutoresizingMaskIntoConstraints = false
+        discordButton.setBackgroundImage(UIImage(named: "socialButtonBackground"), for: .normal)
+        discordButton.setImage(UIImage(named: "discordLogo"), for: .normal)
+        discordButton.setTitle("Discord", for: .normal)
+        discordButton.setTitleColor(UIColor.init(hexString: "#191817"), for: .normal)
+        discordButton.titleLabel?.font = UIFont.systemFont(ofSize: 17.4)
+        return discordButton
+    }()
+    
+    private let twitterButton: UIButton = {
+        let twitterButton = UIButton()
+        twitterButton.translatesAutoresizingMaskIntoConstraints = false
+        twitterButton.setBackgroundImage(UIImage(named: "socialButtonBackground"), for: .normal)
+        twitterButton.setImage(UIImage(named: "twitterLogo"), for: .normal)
+        twitterButton.setTitle("Twitter", for: .normal)
+        twitterButton.setTitleColor(UIColor.init(hexString: "#191817"), for: .normal)
+        twitterButton.titleLabel?.font = UIFont.systemFont(ofSize: 17.4)
+        return twitterButton
+    }()
+    
+    private let githubButton: UIButton = {
+        let githubButton = UIButton()
+        githubButton.translatesAutoresizingMaskIntoConstraints = false
+        githubButton.setBackgroundImage(UIImage(named: "socialButtonBackground"), for: .normal)
+        githubButton.setImage(UIImage(named: "githubLogo"), for: .normal)
+        githubButton.setTitle("GitHub", for: .normal)
+        githubButton.setTitleColor(UIColor.init(hexString: "#191817"), for: .normal)
+        githubButton.titleLabel?.font = UIFont.systemFont(ofSize: 17.4)
+        return githubButton
+    }()
+    
+    private let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(red:0.85, green:0.85, blue:0.85, alpha:1.0)
+        view.backgroundColor = UIColor(red:0.85, green:0.85, blue:0.85, alpha:1.0)
+        
+        view.addSubview(balanceLogoImageView)
+        balanceLogoImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.topMargin).offset(15)
+            make.centerX.equalToSuperview()
+        }
+        
+        view.addSubview(balanceNameImageView)
+        balanceNameImageView.snp.makeConstraints { make in
+            make.top.equalTo(balanceLogoImageView.snp.bottom).offset(20)
+            make.centerX.equalTo(balanceLogoImageView)
+        }
+        
+        view.addSubview(linesImageView)
+        linesImageView.snp.makeConstraints { make in
+            make.top.equalTo(balanceNameImageView.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+        }
+        
+        view.addSubview(richardImageView)
+        richardImageView.snp.makeConstraints { make in
+            make.centerX.equalTo(linesImageView)
+            make.centerY.equalTo(linesImageView)
+        }
+        
+        view.addSubview(joinTitleLabel)
+        joinTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(linesImageView.snp.bottom).offset(5)
+            make.centerX.equalToSuperview()
+        }
+        
+        view.addSubview(joinSubtitleLabel)
+        joinSubtitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(joinTitleLabel.snp.bottom).offset(5)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.height.equalTo(60)
+        }
+        
+        mediumButton.addTarget(self, action: #selector(socialButtonAction(_:)), for: .touchUpInside)
+        view.addSubview(mediumButton)
+        mediumButton.snp.makeConstraints { make in
+            make.top.equalTo(joinSubtitleLabel.snp.bottom).offset(15)
+            make.centerX.equalToSuperview().multipliedBy(0.5)
+            make.width.equalTo(167)
+            make.height.equalTo(47)
+        }
+        mediumButton.imageView?.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(19)
+            make.centerY.equalToSuperview()
+        }
+        
+        discordButton.addTarget(self, action: #selector(socialButtonAction(_:)), for: .touchUpInside)
+        view.addSubview(discordButton)
+        discordButton.snp.makeConstraints { make in
+            make.top.equalTo(mediumButton)
+            make.centerX.equalToSuperview().multipliedBy(1.5)
+            make.width.equalTo(mediumButton)
+            make.height.equalTo(mediumButton)
+        }
+        discordButton.imageView?.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(15)
+            make.centerY.equalToSuperview()
+        }
+        
+        twitterButton.addTarget(self, action: #selector(socialButtonAction(_:)), for: .touchUpInside)
+        view.addSubview(twitterButton)
+        twitterButton.snp.makeConstraints { make in
+            make.top.equalTo(mediumButton.snp.bottom).offset(10)
+            make.centerX.equalTo(mediumButton)
+            make.width.equalTo(mediumButton)
+            make.height.equalTo(mediumButton)
+        }
+        twitterButton.imageView?.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(19)
+            make.centerY.equalToSuperview()
+        }
+
+        githubButton.addTarget(self, action: #selector(socialButtonAction(_:)), for: .touchUpInside)
+        view.addSubview(githubButton)
+        githubButton.snp.makeConstraints { make in
+            make.top.equalTo(twitterButton)
+            make.centerX.equalTo(discordButton)
+            make.width.equalTo(mediumButton)
+            make.height.equalTo(mediumButton)
+        }
+        githubButton.imageView?.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+        }
+        
+        tableView.backgroundColor = .white
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 44
+        tableView.isScrollEnabled = false
+        tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: "settingsCell")
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(githubButton.snp.bottom).offset(15)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(tableView.rowHeight * 4)
+        }
+    }
+    
+    // MARK: - Button Actions -
+    
+    @objc private func socialButtonAction(_ sender: UIButton) {
+        var urlString: String?
+        switch sender {
+        case mediumButton:
+            urlString = "https://medium.com/balance-io"
+        case discordButton:
+            urlString = "https://discord.gg/KR7hDR7"
+        case twitterButton:
+            urlString = "https://twitter.com/Balance_io"
+        case githubButton:
+            urlString = "https://github.com/balance-io"
+        default:
+            break
+        }
+        if let urlString = urlString, let url = URL(string: urlString) {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    // MARK: - Table View -
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath) as! SettingsTableViewCell
+        cell.isEnabled = false
+        cell.sideLabel.text = ""
+        switch indexPath.row {
+        case 0:
+            cell.imageView?.image = UIImage(named: "notificationsIcon")
+            cell.textLabel?.text = "Notifications"
+        case 1:
+            cell.imageView?.image = UIImage(named: "proIcon")
+            cell.textLabel?.text = "Balance Pro"
+        case 2:
+            cell.isEnabled = true
+            cell.imageView?.image = UIImage(named: "currencyIcon")
+            cell.textLabel?.text = "Currency"
+            cell.sideLabel.text = "USD"
+        case 3:
+            cell.imageView?.image = UIImage(named: "languageIcon")
+            cell.textLabel?.text = "Language"
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 100
+//    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("Row: \(indexPath.row)")
     }
 }
