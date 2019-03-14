6664fd3e229e926971078c0732584e4b7cc8f88d
Ben Baron <ben@einsteinx2.com>
2019-03-14 01:15:46 -0500

    Fixed keyboard hides add wallet button
    closes https://github.com/balance-io/balance-ios/issues/19

--- AddEthereumWalletViewController.swift
+++ AddEthereumWalletViewController.swift
@@ -368,7 +368,7 @@ class AddEthereumWalletViewController: UIViewController, UITextFieldDelegate, AV
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= (keyboardSize.height - 150)
+                self.view.frame.origin.y -= keyboardSize.height
                 print("y: \(self.view.frame.origin.y) keyboardHeight: \(keyboardSize.height)")
             }
         }
