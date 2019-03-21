adaed29cfab24ad417356aedf1c6b44daf06665f
Jamie Rumbelow <jamie@jamierumbelow.net>
2019-03-21 17:22:11 +0100

    Adjusting access level for non-overridden WalletTableViewCell properties

--- Balance/Table Cells/WalletTableViewCell.swift
+++ Balance/Table Cells/WalletTableViewCell.swift
@@ -15,11 +15,11 @@ class WalletTableViewCell: UITableViewCell {
             guard let walletItem = wallet else {
                 return
             }
-            
+
             walletWasSet(walletItem: walletItem)
         }
     }
-    
+
     internal let addressLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 12)
@@ -28,24 +28,24 @@ class WalletTableViewCell: UITableViewCell {
         label.textAlignment = .left
         return label
     }()
-    
-    internal let containerView: UIView = {
+
+    private let containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .clear
         return view
     }()
-    
-    internal let backgroundImageView: UIImageView = {
+
+    private let backgroundImageView: UIImageView = {
         let backgroundImageView = UIImageView()
         backgroundImageView.image = UIImage(named: "walletCellBackground")?.stretchableImage(withLeftCapWidth: 20, topCapHeight: 20)
         return backgroundImageView
     }()
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+
         contentView.backgroundColor = UIColor(hexString: "#fbfbfb")
-        
+
         contentView.addSubview(containerView)
         containerView.snp.makeConstraints { make in
             // NOTE: Setting priority to less than 1000 on the top and bottom constraints prevents a constraint error when removing the cell
@@ -54,7 +54,7 @@ class WalletTableViewCell: UITableViewCell {
             make.trailing.equalToSuperview().offset(-10)
             make.bottom.equalToSuperview().offset(-10).priority(999)
         }
-        
+
         containerView.addSubview(backgroundImageView)
         backgroundImageView.snp.makeConstraints { make in
             make.top.equalToSuperview()
@@ -62,26 +62,26 @@ class WalletTableViewCell: UITableViewCell {
             make.trailing.equalToSuperview()
             make.bottom.equalToSuperview()
         }
-        
+
         self.renderTableViewCellContentFor(containerView: containerView)
-        
+
         containerView.setNeedsLayout()
         containerView.layoutIfNeeded()
     }
-    
+
     internal func renderTableViewCellContentFor(containerView: UIView) {
         fatalError("unimplemented")
     }
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         self.selectedBackgroundView?.backgroundColor = selected ? .red : nil
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("unimplemented")
     }
-    
+
     internal func walletWasSet(walletItem: EthereumWallet) {
         addressLabel.text = walletItem.address
     }
